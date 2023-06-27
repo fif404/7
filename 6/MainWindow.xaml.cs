923079f88f78c57eaa2bfed2077ff8b2103e2e93
fifxx <fifxx@DESKTOP-C17PABK>
2023-06-27 16:59:46 +0200

    Добавьте файлы проекта.

---
+++ 6/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _6
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string text = textBox.Text;
+            int tovarsCount = 0;
+
+            CultureInfo culture = CultureInfo.CurrentCulture;
+
+            if (text.IndexOf("Ручка шариковая") != -1)
+            {
+                foreach (string s in text.Split())
+                {
+                    if (s == "Р" || s == "шариковая")
+                    {
+                        tovarsCount++;
+                    }
+                }
+            }
+
+            tovarCountText.Text = tovarsCount.ToString(culture);
+        }
+    }
+}
